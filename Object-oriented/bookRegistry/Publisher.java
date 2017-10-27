9c1ca2e028e843af376b679d970918a1741e97d5
Sabrina B <sabrinabgbc@users.noreply.github.com>
2017-10-27 20:07:52 -0200

    Update Publisher.java

--- Object-oriented/bookRegistry/Publisher.java
+++ Object-oriented/bookRegistry/Publisher.java
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 package bookRegistry;
 
 public class Publisher {
-	private String name;
-=======
-//package bookRegistry;
-
-public class Publisher {
 	public String name;
->>>>>>> b175442ccc05a5cfb9ed7d0c0879b556fd78e85f
 	
 	public void setName(String name) {
 		this.name = name;
