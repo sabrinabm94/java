ba4eb1c0d1ef976c4c062e30431e85a2523b5888
SABRINA BOING MOREIRA <sabrinamoreira@univille.br>
2018-03-23 20:38:35 -0300

    ajuste ping pong

--- data-structure/PingPong/PingPong.java
+++ data-structure/PingPong/PingPong.java
@@ -1,10 +1,8 @@
 public class PingPong extends Thread {
 	private String myWord;
-	
 	public PingPong(String myWord) {
 		this.myWord = myWord;
 	}
-	
 	public void run() {
 		for(int i=0; i<10; i++) {
 			System.out.print(myWord + " ");
@@ -16,11 +14,19 @@ public class PingPong extends Thread {
 		}
 	}
 	
-	public static void main(String [] args) {
+//	public static void main(String [] args) {
+//		Thread t1 = new PingPong("ping");
+//		Thread t2 = new PingPong("pong");
+//		t1.start();
+//		t2.start();
+//		System.out.print("Main function finalized! \n");
+//	}
+	public static void main(String [] args) throws InterruptedException {
 		Thread t1 = new PingPong("ping");
 		Thread t2 = new PingPong("pong");
 		t1.start();
 		t2.start();
-		System.out.print("Main function finalized! \n");
+		t1.join();
+		t2.join();
 	}
 }
