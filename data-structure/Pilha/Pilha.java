be0adb13543deb5955e10eec135f874ca5922713
SABRINA BOING MOREIRA <sabrinamoreira@univille.br>
2018-04-09 20:10:09 -0300

    pilha

--- data-structure/Pilha/Pilha.java
+++ data-structure/Pilha/Pilha.java
@@ -1,5 +1,11 @@
 package pilha;
-
 public class Pilha {
-
+	public static void main(String[] args) {
+		PilhaEstatica p = new PilhaEstatica(5);
+		for(int i = 0; i <= 6; i++) {
+			p.push(i); //adicionando elementos na pilha
+		}
+		p.pop(); //remove um elemento da pilha
+		p.clear(); //remove elementos da pilha enquanto ela não estiver vazia
+	}
 }
--- data-structure/Pilha/PilhaEstatica.java
+++ data-structure/Pilha/PilhaEstatica.java
@@ -1,33 +1,37 @@
 package pilha;
-
 public class PilhaEstatica {
 	private int top;
-	Object pilha[];
-	
+	private Object pilha[];
 	public PilhaEstatica(int capacidade) {
 		pilha = new Object[capacidade];
 		top = 0;		
 	}
-	
 	public void push(Object o) {
 		if(isFull() == false) {
+			System.out.println("Adicionando elemento na pilha...");
 			pilha[top] = o;
 			top = top + 1;
 		} else {
 			System.out.println("Não tem espaço para novos elementos, a pilha alcançou sua capacidade!");
 		}
 	}
-	
 	public Object pop() {
-		pilha[capacidade].pop();	
-	}
-	
-	public void clear() {
-		while(isFull() == true){
-			pilha[capacidade].pop();
+		if(isEmpty() == false) {
+			System.out.println("Retirando elemento da pilha...");
+			top = top - 1;
+			if(top == 0) {
+				System.out.println("A pilha está vazia!");
+			}
+			return pilha[top];
+		} else {
+			return null;
 		}
 	}
-	
+	public void clear() {
+		while(isEmpty() == false){
+			pop();
+		}
+	}
 	public boolean isFull() {
 		if(top == pilha.length){
 			return true;
@@ -36,7 +40,6 @@ public class PilhaEstatica {
 		}   
 		
 	}
-	
 	public boolean isEmpty() {
 		if(top == 0){
 			return true;
@@ -44,5 +47,4 @@ public class PilhaEstatica {
 			return false;
 		}
 	}
-
 }
