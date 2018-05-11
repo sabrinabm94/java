a8f83b1f5643df2a5cd9f60eee446dc7ff00f8ce
SABRINA BOING MOREIRA <sabrinamoreira@univille.br>
2018-05-11 20:00:29 -0300

    queue

---
+++ data-structure/Queue/Main.java
@@ -0,0 +1,18 @@
+package fila;
+
+public class Main {
+		public static void main(String[] args) {
+			Queue queue = new Queue(6); //instance the stack
+			queue.add(20.0); //add items in stack
+			queue.add(20.8);
+			queue.add(20.3);
+			queue.add(44.5);
+			queue.add(33.33);
+			queue.add(20.9);
+			queue.show();
+			queue.remove();
+			queue.remove();
+			queue.show();
+			queue.clear(); //remove one per one items in stack
+	}
+}
