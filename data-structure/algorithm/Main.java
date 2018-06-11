a70425f0ea7947ed462038e8dce1dab089fa9619
SABRINA BOING MOREIRA <sabrinamoreira@univille.br>
2018-06-11 20:27:34 -0300

    algorithm with max e min function

--- data-structure/algorithm/Main.java
+++ data-structure/algorithm/Main.java
@@ -1,5 +1,3 @@
-package algoritmos;
-
 import java.util.Scanner;
 
 public class Main {
@@ -12,6 +10,7 @@ public class Main {
 		int[] list = new int[capacity];
 		enterList(list, number, scanner);
 		returnMax(list);
+		returnMin(list);
 	}	
 	public static void enterList(int list[], int number, Scanner scanner) {
 		for(int j = 0; j < list.length; j++) {
@@ -23,12 +22,22 @@ public class Main {
 	public static int returnMax(int list[]) {
 		int max = list[0];
 		for (int i = 1; i < list.length; i++) {
-			if (list[i] > max) { //worse situation: the vector is sorted decreasingly
-				max = list[i]; //better situation: the vector is sorted increasingly
-			}
+			if (list[i] > max) { //worse case: the vector is sorted increasingly ex. 0,1,2,3,4,5
+				max = list[i]; //better case: the vector is sorted decreasingly  ex. 5,4,3,2,10
+			}                 //medium case: the vector have both ex. 0,1,2,5,4,3
 		}
 		System.out.println("The max number is: " + max);
 		return max;
+	}
+	public static int returnMin(int list[]) {
+		int min = list[0];
+		for (int i = 1; i < list.length; i++) {
+			if (list[i] < min) {
+				min = list[i];
+			}
+		}
+		System.out.println("The min number is: " + min);
+		return min;
 	}
 	//complexity: the most frequent number in the operation (is not a constant)
 	//f(n) = n - 1
