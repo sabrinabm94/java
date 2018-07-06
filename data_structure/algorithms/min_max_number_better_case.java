717e1e286884fd5185a02ac8c5a464e8995775c2
SABRINA BOING MOREIRA <sabrinamoreira@univille.br>
2018-07-06 20:52:02 -0300

    comentários

--- data_structure/algorithms/min_max_number_better_case.java
+++ data_structure/algorithms/min_max_number_better_case.java
@@ -24,29 +24,24 @@ public class min_max_number_better_case {
 		int b; //the high value in the comparation
 		int maxNumber = Integer.MIN_VALUE; //this number can go to infinit positive number
 		int minNumber = Integer.MAX_VALUE; //this number can go to infinit negative number
-		for (i = 0; i < list.length; i += 2) { // n / 2 becase it is comparing 2 number per time
-			if(list[i + 1] <= list[list.length]) {
-				if (list[i] < list[i + 1]) { //compare the first number with the next to one
-					a = i; //the lowest value is i;
-					b = i + 1; //the high value is next value;
-				}                
-				else {
-					a = i + 1; //the lowest value is next value; 
-					b = i; //the high value is i;
-				}
-				if(list[a] < minNumber) { //if the low value of comparation is lower than the minNumber
-					minNumber = list[a]; //this number will be the new minNumber				
-				}
-				if(list[b] > maxNumber) { //if the high number of comparation is biggest than maxNumber
-					maxNumber = list[b]; //this number will be the new maxNumber
-				}
-				//better case: the vector is sorted decreasingly  ex. 5,4,3,2,10
-				//(n-1)
-			} else {
-				System.out.println("End");
-				break;
+		for (i = 0; i < list.length - 1; i += 2) { // n / 2 becase it is comparing 2 number per time
+			if (list[i] < list[i + 1]) { //compare the first number with the next to one
+				a = i; //the lowest value is i;
+				b = i + 1; //the high value is next value;
+			}                
+			else {
+				a = i + 1; //the lowest value is next value; 
+				b = i; //the high value is i;
 			}
-		}
+			if(list[a] < minNumber) { //if the low value of comparation is lower than the minNumber
+				minNumber = list[a]; //this number will be the new minNumber				
+			}
+			if(list[b] > maxNumber) { //if the high number of comparation is biggest than maxNumber
+				maxNumber = list[b]; //this number will be the new maxNumber
+			}//-1 ou não
+			//better case: the vector is sorted decreasingly  ex. 5,4,3,2,10
+			//(n-1)
+		}//impares e atualizar o maior e menor valor comparando com o ultimo do vetor
 		System.out.println("The max number is: " + maxNumber + " the min number is: " + minNumber);
 		return new int[]{maxNumber, minNumber};
 	}
