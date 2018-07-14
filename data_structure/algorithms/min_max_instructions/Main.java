c5a9c70bb92f7789c7a2bff371d9645c96ea2004
Sabrina Boing <sabrinabgbc@email.com>
2018-07-14 14:36:56 -0300

    instruções, contador de casos finalizado

---
+++ data_structure/algorithms/min_max_instructions/Main.java
@@ -0,0 +1,32 @@
+public class Main extends Population {
+	public static void main(String[] args) {
+		Population population = new Population();
+		MinMax minMax = new MinMax();
+		for(int n = 1; n < 100; n++){
+			int[] crescent = new int [n];
+			int[] decrescent = new int [n];
+			int[] random = new int [n];
+			population.crescent(crescent);
+			population.decreasing(decrescent);
+			population.random(random);
+			//minmax1c
+			int op1c = minMax.minMaxWorseCase(crescent);
+			//minmax2c
+			int op2c = minMax.minMaxMediumCase(crescent);
+			//minmax2d
+			int op2d = minMax.minMaxMediumCase(decrescent);
+			//minmax2r
+			int op2r = minMax.minMaxMediumCase(random);
+			//minmax3c
+			int op3 = 0;
+			//even - odd error tratament
+			if(n % 2 == 0) {
+				op3 = minMax.minMaxBetterCaseEven(crescent);
+			} else {
+				op3 = minMax.minMaxBetterCaseOdd(crescent);
+			}
+			//System.out.println(n + ";" + op1 + ";" + op2c + ";" + op2d + ";" + op2a + ";" + op3);
+			//1;0;0;0;0;0
+		}
+	}
+}
---
+++ data_structure/algorithms/min_max_instructions/Population.java
@@ -0,0 +1,20 @@
+public class Population {
+	public void crescent(int v[]){
+		for(int i = 0; i  < v.length; i++){
+			v[i] = i + 1;
+			System.out.println("Crescent - added value: " + v[i]);
+		} 
+	}
+	public void decreasing(int v[]){
+		for(int i = v.length -1;  i >= 0; i--){
+			v[i] = i +1;
+			System.out.println("Decreasing - added value: " + v[i]);
+		}
+	}
+	public void random(int v[]){
+		for(int i = 0; i < v.length; i++){
+			v[i] = (int)(Math.random()*100);	
+			System.out.println("Random - added value: " + v[i]);
+		}		
+	}
+}
