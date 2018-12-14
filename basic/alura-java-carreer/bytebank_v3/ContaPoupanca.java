568afd9a9087204737d89c4bb4a152663137901e
Sabrina Boing Moreira <sabrina.moreira@neogrid.com>
2018-12-14 13:27:27 -0200

    herança e construtores

---
+++ basic/alura-java-carreer/bytebank_v3/ContaPoupanca.java
@@ -0,0 +1,8 @@
+package bytebank_v3;
+
+public class ContaPoupanca extends Conta {
+	public ContaPoupanca(int agencia, int numero) {
+		//passar os parametros para o construtor pai
+		super(agencia, numero);
+	}
+}
