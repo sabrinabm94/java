ed5520c5f23f77fb827bcc886dc1d7a23cb4acd4
sabrina.moreira@neogrid.com <sabrinabgbc@email.com>
2019-07-12 15:59:16 -0300

    tratamento de exceções

---
+++ basic/ErrorHandle/ErrorException.java
@@ -0,0 +1,12 @@
+package ErrorHandle;
+
+public class ErrorException extends Exception {
+    /*
+    Exception
+    Tipo de excessão que é obrigatório ser tratada
+     */
+
+    public ErrorException(String message) {
+        super(message);
+    }
+}
--- basic/ErrorHandle/Main.java
+++ basic/ErrorHandle/Main.java
@@ -1,25 +1,43 @@
 package ErrorHandle;
 
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ErrorException {
+		throwErrorException();
+		throwErrorRuntimeException();
+		cathError();
+	}
+
+	public static void cathError() {
 		int i = 4;
 		int j = 0;
 		int x;
-		
+
 		try {
-			x = i/j;
+			x = i / j;
 		}
-		
-		catch (ArithmeticException a){
-			System.out.println("Division by 0");
+
+		catch (ArithmeticException exception){
+			System.out.println("Division by 0: " + exception);
 		}
-		
-		catch (Exception e) {
-			System.out.println("ErrorHandle.Main: " + e.getMessage());
+
+		catch (RuntimeException error) {
+			System.out.println("Runtime Exception: " + error.getMessage());
 		}
-		
+
+		catch (Exception error) {
+			System.out.println("Exception: " + error.getMessage());
+		}
+
 		finally {
-			System.out.println("Always run");
+			System.out.println("Finally block: always run");
 		}
+	}
+
+	public static void throwErrorException() throws ErrorException {
+		throw new ErrorException("Exception");
+	}
+
+	public static void throwErrorRuntimeException() {
+		throw new ErrorRuntimeException("Runtime Exception");
 	}
 }
