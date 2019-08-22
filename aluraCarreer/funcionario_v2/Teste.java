fd7b34aa5ed4b99097672604a5e0502f4be123de
Sabrina Boing <sabrinabmoreira@gmail.com>
2019-08-22 15:30:12 -0300

    atualização de readme

--- aluraCarreer/funcionario_v2/Teste.java
+++ aluraCarreer/funcionario_v2/Teste.java
@@ -11,19 +11,18 @@ public class Teste {
 		sivy.setCpf("000000000000");
 		sivy.setSalario(2500);
 
-		Print.print("Nome: " + sivy.getNome());
-		Print.print("Salário: " + sivy.getSalario());
-		Print.print("Valor da bonificação: " + sivy.getBonificacao());
+		Print.println("Nome: " + sivy.getNome());
+		Print.println("Salário: " + sivy.getSalario());
+		Print.println("Valor da bonificação: " + sivy.getBonificacao());
 		
 		Funcionario alexander = new Funcionario();
 		alexander.setNome("Alexander C. Becker");
 		alexander.setCpf("111111111111");
 		alexander.setSalario(3000);
 
-		Print.print("");
-		Print.print("Nome: " + alexander.getNome());
-		Print.print("Salário: " + alexander.getSalario());
-		Print.print("Valor da bonificação: " + alexander.getBonificacao());
+		Print.println("\nNome: " + alexander.getNome());
+		Print.println("Salário: " + alexander.getSalario());
+		Print.println("Valor da bonificação: " + alexander.getBonificacao());
 
 		Gerente liv = new Gerente();
 		liv.setNome("Liv B. Becker");
@@ -31,11 +30,10 @@ public class Teste {
 		liv.setSalario(5000);
 		liv.setSenha(12345);
 
-		Print.print("");
-		Print.print("Nome: " + liv.getNome());
-		Print.print("Salário: " + liv.getSalario());
-		Print.print("Valor da bonificacão: " + liv.getBonificacao());
-		Print.print("Autenticado: " + liv.autentica(12345));
+		Print.println("\nNome: " + liv.getNome());
+		Print.println("Salário: " + liv.getSalario());
+		Print.println("Valor da bonificacão: " + liv.getBonificacao());
+		Print.println("Autenticado: " + liv.autentica(12345));
 		
 		//Polimorfismo
 		//Utilização de uma referência mais genérica: funciona pois o gerente é um tipo de funcionário, as funções serão somente as que existem em funcionário
@@ -44,14 +42,15 @@ public class Teste {
 		Gerente gerenteTeste = new Gerente();
 		gerenteTeste.setNome("Marcos");
 		gerenteTeste.setSalario(1500);
+		
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(gerenteTeste);
-		Print.print(controle.getSoma() + "");
+		Print.println(controle.getSoma() + "");
 		
 		Funcionario funcionarioTeste = new Funcionario();
 		funcionarioTeste.setSalario(2000);
 		controle.registra(funcionarioTeste);
-		Print.print(controle.getSoma() + "");
+		Print.println(controle.getSoma() + "");
 		
 		/*
 		func.autentica();
